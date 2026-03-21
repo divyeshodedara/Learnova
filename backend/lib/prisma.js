6f49d3427873c908938aed96ae9a7e1aef399938
divyeshodedara <divyeshvisana123@gmail.com>
2026-03-21 14:00:20 +0530

    auth working

--- backend/lib/prisma.js
+++ backend/lib/prisma.js
@@ -1,5 +1,11 @@
+const { Pool } = require('pg');
+const { PrismaPg } = require('@prisma/adapter-pg');
 const { PrismaClient } = require("../generated/prisma");
 
-const prisma = new PrismaClient({});
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({ adapter });
 
 module.exports = prisma;
